9f49fc7d2d08f249606e2fbffb4fa89fd2d0c043
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-08-12 20:22:00 -0400

    first middleware example

--- app.js
+++ app.js
@@ -2,11 +2,19 @@ const express = require("express");
 const app = express();
 const PORT = 4001;
 
+//routes:
 app.get("/", (req, res) => {
   console.log("Hello, Jerry");
   res.send("Hello, Newman");
 });
 
+//middleware:
+const myLogger = (req, res, next) => {
+  console.log("LOGGED");
+  next();
+};
+
+//listener:
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}....`);
 });
